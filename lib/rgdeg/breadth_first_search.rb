03e4caf184cf44f76f8a5fcda8cf8855b9ebe8d6
David Grayson <davidegrayson@gmail.com>
2018-02-18 23:58:34 -0800

    Duh, breadth first search should have visitor tracking.

--- lib/rgdeg/breadth_first_search.rb
+++ lib/rgdeg/breadth_first_search.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 module Rgdeg
   # Easy modifications:
   #
@@ -10,16 +12,22 @@ module Rgdeg
   # We can't do that in the main version because it breaks applications that
   # using nodes that are arrays.
   #
+  # Multiple visits: If you don't care about the same node being searched
+  # multiple times, just remove the three lines of code that refer to 'visited'.
+  #
   # Avoid passing a block: If you want the results of the search to be returned
   # as an enumerator, use Ruby's enum_for method.  You can then then just use
   # that enumerator, or call Ruby's to_a method to convert it to an array:
   #
   #   enum_for(:breadth_first_search_dag, graph, start).to_a
   #
-  def breadth_first_search_dag(graph, start)
+  def breadth_first_search(graph, start)
     queue = [start]
+    visited = Set.new
     until queue.empty?
       node = queue.shift
+      next if visited.include?(node)
+      visited << node
       queue.concat(graph.fetch(node))
       yield node
     end
