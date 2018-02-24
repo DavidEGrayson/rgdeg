46879eba7b80c94b6adeeac00556078117687158
David Grayson <davidegrayson@gmail.com>
2018-02-23 19:45:26 -0800

    Simplify transitive_reduction even more.

--- lib/rgdeg/transitive_reduction.rb
+++ lib/rgdeg/transitive_reduction.rb
@@ -8,24 +8,12 @@ module Rgdeg
   # This algorithm only works with DAGs.  It will detect cycles and raise an
   # exception.
   def transitive_reduction(graph)
-    # Given a set or array of nodes, returns the set of nodes that can be
-    # reached from those nodes by following one edge.
-    follow_one_edge_for_set_of_nodes = lambda do |nodes|
-      new_nodes = Set.new
-      nodes.each do |node|
-        graph.fetch(node).each do |child|
-          new_nodes << child
-        end
-      end
-      new_nodes
-    end
-
     tr = {}
     graph.each do |start_node, nodes|
       nodes_with_max_distance_1 = Set.new(nodes)
       distance = 1
       until nodes.empty?
-        nodes = follow_one_edge_for_set_of_nodes.(nodes)
+        nodes = Set.new nodes.flat_map &graph.method(:fetch)
         distance += 1
         nodes_with_max_distance_1 -= nodes
         if distance > graph.size
